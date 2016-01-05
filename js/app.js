9f451ff1b86af47c4ddd0d8389242c2cc83e1898
amirch1 <amir.chervinsky@kaltura.com>
2016-01-05 18:02:46 +0200

    add sliders

--- js/app.js
+++ js/app.js
@@ -1,11 +1,33 @@
+function setupSliders(){
+	$(".brightnessFilter").simpleSlider({range: [0,10], step: 0.1});
+	$(".contrastFilter").simpleSlider({range: [0,10], step: 0.1});
+	$(".grayscaleFilter").simpleSlider({range: [0,1], step: 0.1});
+	$(".saturationFilter").simpleSlider({range: [0,10], step: 0.1});
+	$(".hueFilter").simpleSlider({range: [0,360], step: 30});
+	$(".sepiaFilter").simpleSlider({range: [0,1], step: 0.1});
+}
+
+function resetFilters() {
+	$(".brightnessFilter").simpleSlider("setValue", 1);
+	$(".contrastFilter").simpleSlider("setValue", 1);
+	$(".grayscaleFilter").simpleSlider("setValue", 0);
+	$(".saturationFilter").simpleSlider("setValue", 1);
+	$(".hueFilter").simpleSlider("setValue", 0);
+	$(".sepiaFilter").simpleSlider("setValue", 0);
+}
+
 window.onload = function() {
-	var settingsBoxOpened = false;
+	setupSliders();
+	resetFilters();
+
 	$(".settingsBoxIcon").on("click", function(){
-		settingsBoxOpened = !settingsBoxOpened;
-		if (settingsBoxOpened){
-			$(".settingsBox, .settingsBoxIcon").addClass("open");
-		}else{
-			$(".settingsBox, .settingsBoxIcon").removeClass("open");
-		}
+		$(".settingsBox, .settingsBoxIcon").toggleClass("open");
 	});
+
+	$(".filtersResetBtn").on("click", function(){
+		resetFilters();
+	});
+
+
+
 };
